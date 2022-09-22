87b32af1b5c14c72f1286f5541d3876d77ec759b
devcxl <orange.cxl@qq.com>
2022-09-22 11:38:08 +0800

    fix: add all

--- src/test/java/cn/devcxl/common/WebConfig.java
+++ src/test/java/cn/devcxl/common/WebConfig.java
@@ -20,4 +20,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor).addPathPatterns("/**");
     }
+
+
 }
